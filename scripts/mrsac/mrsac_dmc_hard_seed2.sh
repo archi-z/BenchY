6c14f99790b310f9b7eb05e6536a1b14a2137203
archi <1036991178@qq.com>
2025-05-03 11:10:24 +0800

    【模块/问题名称】：测试脚本 【变更描述】：添加测试脚本

---
+++ scripts/mrsac/mrsac_dmc_hard_seed2.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+CUDA_VISIBLE_DEVICES=0
+env_name="dog-run humanoid-run"
+seed=2
+n_step=3
+encoder_horizon=5
+
+for en in ${env_name};
+do
+    CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES} \
+    python run_mr.py \
+        --overrides \
+            agent="mrsac" \
+            env="dmc_hard" \
+            env.env_name=${en} \
+            seed=${seed} \
+            n_step=${n_step} \
+            agent.encoder_horizon=${encoder_horizon} \
+            updates_per_interaction_step=1
+done
